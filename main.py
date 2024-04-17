f56e6dadaa5f1517be04f1df9064bdd02de639e4
Andrew Oliveira <andrew.oliveira@enacom.com.br>
2024-04-17 12:24:44 -0300

    Removendo colunas que nã serão utilizadas

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import pandas as pd
 
-file_name = './hus.csv'
+def csv2mark(file_csv):
+    table_md = file_csv.to_markdown(index=False)
+    
+    with open('tabela.md', 'w') as file:
+        file.write(table_md)
+        
+    return table_md
+
+def user2devops_user():
+    pass
+# nome do arquivo
+file_name = './HUs.csv'
 
 data = pd.read_csv(file_name)
-
+# Linhas abaixo realiaza tratamento especifico do arquivo que estou trabalhando
 data = data.drop('Work Item Type', axis=1)
+data = data.drop('Assigned To', axis=1)
 
-table_md = data.to_markdown(index=False)
 
-with open('tabela.md', 'w') as file:
-    file.write(table_md)
+data_mkd = csv2mark(data)
 
 print("Tabela Markdown salva com sucesso no arquivo 'tabela.md'")
